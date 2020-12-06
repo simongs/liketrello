0a9442258c92c44efe21953368ec5c58e6738f8d
chopokmado <chopokmado@gmail.com>
2020-12-06 23:07:00 +0900

    Initial commit

---
+++ src/main/java/com/dasolute/simons/liketrello/LiketrelloApplication.java
@@ -0,0 +1,13 @@
+package com.dasolute.simons.liketrello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiketrelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiketrelloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dasolute/simons/liketrello/LiketrelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dasolute.simons.liketrello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiketrelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
